11816bfaa18dd1d69c075cb9d19b6a702cb3f63e
marco bubola <marcobubola@hotmail.com>
2026-04-29 19:53:55 -0300

    Create spacing_tokens.dart

---
+++ lib/core/design_system/tokens/spacing_tokens.dart
@@ -0,0 +1,25 @@
+abstract class AppSpacing {
+  static const double xs = 4;
+  static const double sm = 8;
+  static const double md = 12;
+  static const double lg = 16;
+  static const double xl = 24;
+  static const double xxl = 32;
+  static const double xxxl = 48;
+}
+
+abstract class AppRadius {
+  static const double card = 10;
+  static const double modal = 14;
+  static const double pill = 999;
+  static const double input = 10;
+  static const double button = 10;
+  static const double small = 6;
+}
+
+abstract class AppElevation {
+  static const double none = 0;
+  static const double low = 1;
+  static const double medium = 3;
+  static const double high = 6;
+}
