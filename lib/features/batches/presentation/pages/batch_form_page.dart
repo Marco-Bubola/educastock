f001508dba1809849965312c0af59b2cf38b33dc
marco bubola <marcobubola@hotmail.com>
2026-04-29 20:57:25 -0300

    Update batch_form_page.dart

--- lib/features/batches/presentation/pages/batch_form_page.dart
+++ lib/features/batches/presentation/pages/batch_form_page.dart
@@ -3,7 +3,6 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:intl/intl.dart';
 import '../../../../core/design_system/design_system.dart';
-import '../../../../core/router/app_router.dart';
 import '../../../auth/presentation/controllers/auth_provider.dart';
 import '../../domain/entities/batch.dart';
 import '../controllers/batches_provider.dart';
