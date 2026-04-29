bf1000f9896360bfb5229c6be41e47a2e9576851
marco bubola <marcobubola@hotmail.com>
2026-04-29 20:57:40 -0300

    Update alerts_page.dart

--- lib/features/alerts/presentation/pages/alerts_page.dart
+++ lib/features/alerts/presentation/pages/alerts_page.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:go_router/go_router.dart';
 import 'package:intl/intl.dart';
 import '../../../../core/design_system/design_system.dart';
-import '../../../../core/router/app_router.dart';
 import '../../../batches/presentation/controllers/batches_provider.dart';
 
 class AlertsPage extends ConsumerWidget {
